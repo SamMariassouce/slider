03163723d668e53b0ff21ad92408f625b5dc0537
Sam Mariassouce <sammariassouce@gmail.com>
2023-05-25 17:01:51 -1000

    slider and slick sider

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Carousel from './Carousel';
+import SlickCaroussel from './SlickCaroussel';
+
+const App = () => {
+  return (
+    <main>
+      <Carousel />
+      <SlickCaroussel />
+    </main>
+  );
+};
+export default App;
